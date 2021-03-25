58f463077990877ebbe990750cec009b9c4d2eac
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-03-25 15:58:16 -0300

    Add Admin navbar

--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -9,7 +9,7 @@ const Home = () => (
         <div className="row home-content card-base border-radius-20">
             <div className="col-6 home-text">
                 <h1 className="text-title">
-                    Conheça o melhor <br/> catálogo de produtos
+                    Conheça o melhor <br /> catálogo de produtos
                 </h1>
                 <p className="text-subtitle">
                     Ajudaremos você a encontrar os melhores <br/> produtos disponíveis no mercado.
