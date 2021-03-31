3b9a1957b20396a2caa6328c68d6f200f0d847c4
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-03-31 19:43:20 -0300

    UserUpdateValidator

---
+++ backend/src/main/java/com/sidneyrod/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,9 @@
+package com.sidneyrod.dscatalog.dto;
+
+import com.sidneyrod.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+	private static final long serialVersionUID = 1L;
+	
+}
--- backend/src/main/java/com/sidneyrod/dscatalog/services/UserService.java
+++ backend/src/main/java/com/sidneyrod/dscatalog/services/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.sidneyrod.dscatalog.dto.RoleDTO;
 import com.sidneyrod.dscatalog.dto.UserDTO;
 import com.sidneyrod.dscatalog.dto.UserInsertDTO;
+import com.sidneyrod.dscatalog.dto.UserUpdateDTO;
 import com.sidneyrod.dscatalog.entities.Role;
 import com.sidneyrod.dscatalog.entities.User;
 import com.sidneyrod.dscatalog.repositories.RoleRepository;
@@ -58,7 +59,7 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserDTO update(Long id, UserDTO dto) {
+	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
 		User entity = repository.getOne(id);
 		copyDtoToEntity(dto, entity);
