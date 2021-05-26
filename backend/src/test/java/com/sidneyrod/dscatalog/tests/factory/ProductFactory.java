bff26f850e9c127fc91c04e3edea53ac89c083d6
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-05-26 15:44:34 -0300

    ProductFactory update

--- backend/src/test/java/com/sidneyrod/dscatalog/tests/factory/ProductFactory.java
+++ backend/src/test/java/com/sidneyrod/dscatalog/tests/factory/ProductFactory.java
@@ -3,15 +3,25 @@ package com.sidneyrod.dscatalog.tests.factory;
 import java.time.Instant;
 
 import com.sidneyrod.dscatalog.dto.ProductDTO;
+import com.sidneyrod.dscatalog.entities.Category;
 import com.sidneyrod.dscatalog.entities.Product;
 
 public class ProductFactory {
 
 	public static Product createProduct() {
-		return new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2021-04-28T03:00:00Z"));
+		Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2021-05-25T03:00:00Z"));
+		product.getCategories().add(new Category(1L, null));
+		return product;
 	}
 	
 	public static ProductDTO createProductDTO() {
-		return new ProductDTO(createProduct());
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+	
+	public static ProductDTO createProductDTO(Long id) {
+		ProductDTO dto = createProductDTO();
+		dto.setId(id);
+		return dto;
 	}
 }
