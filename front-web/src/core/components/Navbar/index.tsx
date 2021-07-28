3a53ec57a26ce002df3dea58968630ddea0b2aa4
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-07-27 22:33:15 -0300

    Starting logout process

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -2,33 +2,52 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import './styles.scss';
 
-const Navbar = () => (
-    <nav className="row bg-primary main-nav">
-        <div className="col-2">
-            <Link to="/" className="nav-logo-text">
-                <h4>SID Catalog</h4>
-            </Link>
-        </div>
-        <div className="col-6 offset-2">
-            <ul className="main-menu">
-                <li>
-                    <NavLink to="/" activeClassName="active" exact>
-                        HOME
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/products" activeClassName="active">
-                        CATÁLOGO
-                    </NavLink>
-                </li>
-                <li>
-                <NavLink to="/admin" activeClassName="active">
-                        ADMIN
-                    </NavLink>
-                </li>
-            </ul>
-        </div>
-    </nav>
-);
+const Navbar = () => {
+    const currentUser = '';
+
+    return (
+        <nav className="row bg-primary main-nav">
+            <div className="col-3">
+                <Link to="/" className="nav-logo-text">
+                    <h4>SID Catalog</h4>
+                </Link>
+            </div>
+            <div className="col-6">
+                <ul className="main-menu">
+                    <li>
+                        <NavLink to="/" activeClassName="active" exact className="nav-link">
+                            HOME
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/products" activeClassName="active" className="nav-link">
+                            CATÁLOGO
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/admin" activeClassName="active" className="nav-link">
+                            ADMIN
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className="col-3 text-right">
+                {currentUser && (
+                    <>
+                        {currentUser}
+                        <a ref="#logout" className="nav-link active">
+                            LOGOUT
+                        </a>
+                    </>
+                )}
+                {!currentUser && (
+                    <Link to="/auth/login" className="nav-link active" >
+                        LOGIN
+                    </Link>
+                )}
+            </div>
+        </nav>
+    )
+};
 
 export default Navbar;
