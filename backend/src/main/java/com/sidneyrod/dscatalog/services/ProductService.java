c98c7f87c3e5ed97056b1163c33c88d1a0c09781
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-08-02 23:01:57 -0300

    Fix Product Filters

--- backend/src/main/java/com/sidneyrod/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/sidneyrod/dscatalog/services/ProductService.java
@@ -54,10 +54,6 @@ public class ProductService {
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
 		copyDtoToEntity(dto, entity);
-			if (entity.getCategories().size() == 0) {
-			Category cat = categoryRepository.getOne(1L);
-			entity.getCategories().add(cat);
-			}
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 	}
@@ -67,10 +63,6 @@ public class ProductService {
 		try {
 		Product entity = repository.getOne(id);
 		copyDtoToEntity(dto, entity);
-			if (entity.getCategories().size() == 0) {
-				Category cat = categoryRepository.getOne(1L);
-				entity.getCategories().add(cat);
-				}
 		entity = repository.save(entity);
 		return new ProductDTO(entity);
 		}
