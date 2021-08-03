eed49b2c87859ec2c730f02d8fa9de1bc103bd0a
Sidney Rodrigues <sidney.silva.rodrigues@outlook.com>
2021-08-02 23:16:33 -0300

    Fix findAll ProductResource

--- backend/src/main/java/com/sidneyrod/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/sidneyrod/dscatalog/resources/ProductResource.java
@@ -32,14 +32,12 @@ public class ProductResource {
 	
 	@GetMapping
 	public ResponseEntity<Page<ProductDTO>> findAll(
-			
-			@RequestParam(value = "categoryId", defaultValue = "0") Long categoryId,
-			@RequestParam(value = "name", defaultValue = "") String name,
+			@RequestParam(value = "categoryId", defaultValue = "0") Long categoryId, 
+			@RequestParam(value = "name", defaultValue = "") String name, 
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "id") String orderBy
-			) {
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
 		
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
--- front-web/src/core/components/ProductFilters/index.tsx
+++ front-web/src/core/components/ProductFilters/index.tsx
@@ -6,60 +6,60 @@ import { Category } from 'core/types/Product'
 import './styles.scss'
 
 type Props = {
-  name?: string
-  category?: Category
-  handleChangeName: (name: string) => void
-  handleChangeCategory: (category: Category) => void
-  clearFilters: () => void
+    name?: string
+    category?: Category
+    handleChangeName: (name: string) => void
+    handleChangeCategory: (category: Category) => void
+    clearFilters: () => void
 }
 
-const ProductFilters = ({ name, category, handleChangeName, handleChangeCategory, clearFilters}: Props) => {
-  const [categories, setCategories] = useState<Category[]>([])
-  const [isLoadingCategories, setIsLoadingCategories] = useState(false)
+const ProductFilters = ({ name, category, handleChangeName, handleChangeCategory, clearFilters }: Props) => {
+    const [isLoadingCategories, setIsLoadingCategories] = useState(false)
+    const [categories, setCategories] = useState<Category[]>([])
 
-  useEffect(() => {
-    setIsLoadingCategories(true)
-    makeRequest({ url: '/categories' })
-      .then(response => setCategories(response.data.content))
-      .finally(() => setIsLoadingCategories(false))
-  }, [])
+    useEffect(() => {
+        setIsLoadingCategories(true)
+        makeRequest({ url: '/categories' })
+            .then(response => setCategories(response.data.content))
+            .finally(() => setIsLoadingCategories(false))
+    }, [])
 
-  return (
-    <div className="card-base product-filters-container">
-      <div className="input-search">
-        <input
-          type="text"
-          value={ name }
-          className="form-control"
-          placeholder="Pesquisar produto"
-          onChange={ event => handleChangeName(event.target.value) }
-        />
-        <SearchIcon />
-      </div>
+    return (
+        <div className="card-base product-filters-container">
+            <div className="input-search">
+                <input
+                    type="text"
+                    value={name}
+                    className="form-control"
+                    placeholder="Pesquisar produto"
+                    onChange={event => handleChangeName(event.target.value)}
+                />
+                <SearchIcon />
+            </div>
 
-      <Select
-        name="categories"
-        key={ `select-${category?.id}` }
-        value={ category }
-        isLoading={ isLoadingCategories }
-        options={ categories }
-        getOptionValue={ (option: Category) => String(option.id) }
-        getOptionLabel={ (option: Category) => option.name }
-        className="filter-select-container"
-        classNamePrefix="product-categories-select"
-        placeholder="Categorias"
-        onChange={ value => handleChangeCategory(value as Category) }
-        isClearable
-      />
+            <Select
+                name="categories"
+                key={`select-${category?.id}`}
+                value={category}
+                isLoading={isLoadingCategories}
+                options={categories}
+                getOptionValue={(option: Category) => String(option.id)}
+                getOptionLabel={(option: Category) => option.name}
+                className="filter-select-container"
+                classNamePrefix="product-categories-select"
+                placeholder="Categorias"
+                onChange={value => handleChangeCategory(value as Category)}
+                isClearable
+            />
 
-      <button
-        className="btn btn-outline-secondary font-weight-bold border-radius-10"
-        onClick={ clearFilters }
-      >
-        LIMPAR FILTRO
-      </button>
-    </div>
-  )
+            <button
+                className="btn btn-outline-secondary font-weight-bold border-radius-10"
+                onClick={clearFilters}
+            >
+                LIMPAR FILTRO
+            </button>
+        </div>
+    )
 }
 
 export default ProductFilters
